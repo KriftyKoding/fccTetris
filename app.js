bc404b86db6c2b4c593741c901bcea5d8b26dd4a
Krifty <kaitlynmariejones@gmail.com>
2022-02-05 18:13:00 -0800

    game pieces create add stop dic to game board created differenet oiece randomly select pices display and undisplay piece auto move down auto stop when hit something below

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+//////////////////////////////////////////////////////////////
+///////////////////// DOM Manipulation ///////////////////////
+///////////////////////////////////////////////////////////////
 const domBody = document.body;
-
 // score display Element Create
 const score = document.createElement("h3");
 score.innerText = "Score: ";
@@ -7,24 +9,134 @@ domBody.appendChild(score);
 const scoreDisplay = document.createElement("span");
 scoreDisplay.id = "score";
 scoreDisplay.innerText = "0";
-score.appendChild(scoreDisplay)
+score.appendChild(scoreDisplay);
 
 //start/pause button Element Create
-const startBTTN = document.createElement("button")
-startBTTN.id = "start-button"
-startBTTN.innerText = "Start/Pause"
-domBody.appendChild(startBTTN)
+const startBTTN = document.createElement("button");
+startBTTN.id = "start-button";
+startBTTN.innerText = "Start/Pause";
+domBody.appendChild(startBTTN);
 
 //game container Element Create
-const grid = document.createElement("div")
-grid.classList = "grid"
-domBody.appendChild(grid)
+const grid = document.createElement("div");
+grid.classList = "grid";
+domBody.appendChild(grid);
 //grid container
-for (let i = 0; i < 200; i++){
-    let girdDiv = document.createElement('div')
-    grid.appendChild(girdDiv)
-} 
+let squares = [];
+for (let i = 0; i < 200; i++) {
+  let girdDiv = document.createElement("div");
+  grid.appendChild(girdDiv);
+  squares.push(girdDiv);
+}
+for (let i = 0; i < 10; i++) {
+    let girdDiv = document.createElement("div");
+    girdDiv.classList = "taken";
+    grid.appendChild(girdDiv);
+    squares.push(girdDiv);
+  }
 
+
+
+////////////////////////////////////////////
+//////////////////////////////////////////////
+/////////////////////////////////////////////
+const width = 10; //# of square to next row
+// The Tetrominos
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [(width, width + 1, width + 2, width * 2 + 2)],
+  [(width * 2, width * 2 + 1, width + 1, 1)],
+  [(width, width * 2, width * 2 + 1, width * 2 + 2)],
+];
+const zTetromino = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+];
+
+const tTetromino = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1],
+];
+
+const oTetromino = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+];
+
+const iTetromino = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+];
+
+const theTetrominoes = [
+  lTetromino,
+  zTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino,
+];
+
+let currentPosition = 4
+let currentRotation = 0
+
+let random = Math.floor(Math.random()*theTetrominoes.length)
+let current = theTetrominoes[random][currentRotation]
+
+
+
+
+
+
+//draw tetromino
+function draw () {
+    current.forEach(index => {
+        squares[currentPosition +index].classList.add('tetromino')
+    });
+}
+
+// undraw
+function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+
+  timerID = setInterval(moveDown, 1000)
+
+  //move
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze() 
+  }
+
+  //hit 
+  function freeze () {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //start new tetromino
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+
+  }
+
+   
+
+ 
+
+    
 
 
 
